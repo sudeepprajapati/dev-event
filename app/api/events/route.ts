d5294a71bc74f1f27e1158db485b9c46c1eb1643
Sudeep <sudeeppprajapati@gmail.com>
2025-12-06 16:52:10 +0530

    add image upload functionality using Cloudinary in event creation

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,4 +1,5 @@
 import { Event } from "@/database";
+import { v2 as cloudinary } from 'cloudinary';
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -16,6 +17,26 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ message: 'Invalid JSON data format' }, { status: 400 })
         }
 
+        const file = formData.get('image') as File;
+
+        if (!file || file.size === 0) {
+            return NextResponse.json({ message: 'Image file is required' }, { status: 400 });
+        }
+
+        const arrayBuffer = await file.arrayBuffer();
+
+        const buffer = Buffer.from(arrayBuffer)
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'DevEvent' }, (error, results) => {
+                if (error) return reject(error);
+
+                resolve(results);
+
+            }).end(buffer);
+        })
+
+        event.image = (uploadResult as { secure_url: string }).secure_url;
         const createdEvent = await Event.create(event);
 
         return NextResponse.json({ message: 'Event created successfully', event: createdEvent },
