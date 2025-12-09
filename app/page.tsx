14339cd769a18c0b4b5a98f3acb4b368a9f0042b
Sudeep <sudeeppprajapati@gmail.com>
2025-12-09 15:00:09 +0530

    Change list style

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const page = async () => {
 
         <ul className="events " >
           {events && events.length > 0 && events.map((event: IEvent) => (
-            <li key={event.title}>
+            <li key={event.title} className="list-none">
               <EventCard {...event} />
             </li>
           ))}
