339ea92ae355abfd3fdde56f92ce0236e0a9d029
Sudeep <sudeeppprajapati@gmail.com>
2025-12-15 17:30:09 +0530

    add BookEvent component

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,17 +1,28 @@
 'use client'
 
+import { createBooking } from "@/lib/actions/booking.action"
 import { useState } from "react"
 
-const BookEvent = () => {
+const BookEvent = ({ eventId, slug }: { eventId: string; slug: string }) => {
     const [email, setEmail] = useState('')
     const [submitted, setSubmitted] = useState(false)
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        setTimeout(() => {
-            setSubmitted(true)
-        }, 1000);
-    }
+
+        if (!email) return;
+
+        try {
+            const { success } = await createBooking({ eventId, slug, email });
+
+            if (success) {
+                setSubmitted(true);
+                setEmail('');
+            }
+        } catch (error) {
+            console.error('Booking creation failed')
+        }
+    };
 
     return (
         <div id="book-event">
