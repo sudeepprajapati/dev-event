d021d8a5acd9ec2cae49571e48b525b0fcf33f8a
Sudeep <sudeeppprajapati@gmail.com>
2025-12-16 18:00:18 +0530

    Add Nav Links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                 </Link>
                 <ul>
                     <Link href="/">Home</Link>
-                    <Link href="/events">Events</Link>
+                    <Link href="#events">Events</Link>
                     <Link href="/create-event">Create Event</Link>
                 </ul>
             </nav>
